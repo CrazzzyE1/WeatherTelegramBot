c2e8f12edd13e237b7c50eee9bd45a65867ce4e4
litvakivan <litvakivan@gmail.com>
2020-12-23 22:58:34 +0300

    FINALLY

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,10 +2,17 @@ import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Bot extends TelegramLongPollingBot {
     public static void main(String[] args) {
@@ -19,28 +26,61 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            // Set variables
-            String message_text = update.getMessage().getText();
+        Model model = new Model();
+        String message_text;
+
+        if (update.hasMessage() && (update.getMessage().hasText() || update.getMessage().hasLocation())) {
             long chat_id = update.getMessage().getChatId();
 
-            switch (message_text) {
-                case "/help":
-                    message_text = "What are you want, bitch?";
-                    break;
-                case "/settings":
-                    message_text = "Go to sleep, freak!";
-                    break;
-                case "/about":
-                    message_text = "I'm the best Gparser!";
-                    break;
-                case "/parsing":
-                    message_text = "Not now, motherfucker!";
-                    break;
-                default:
-                    message_text = "I don't understand you, fagot!";
+            if (update.getMessage().hasLocation()) {
+                String lat = update.getMessage().getLocation().getLatitude().toString();
+                String lon = update.getMessage().getLocation().getLongitude().toString();
+//                message_text = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&lang=ru&appid=";
+                message_text = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&lang=ru&appid=";
+            } else {
+                message_text = update.getMessage().getText();
+                switch (message_text) {
+                    case "/start":
+                        message_text = "\uD83C\uDF2A\uD83C\uDF08☀️\uD83C\uDF24⛅️\uD83C\uDF25☁️\uD83C\uDF26\uD83C\uDF27⛈\n" +
+                                "\n" +
+                                "Привет, Бро! \n" +
+                                "Я Погодный Телеграм Бот! \n" +
+                                "Я знаю о погоде ВСЁ. \n" +
+                                "Напиши мне название города, в котором ты хочешь узнать погоду, \n" +
+                                "и я моментально дам тебе ответ!\n" +
+                                "\n" +
+                                "\uD83C\uDF29\uD83C\uDF28❄️☃️⛄️\uD83C\uDF2C\uD83D\uDCA8\uD83D\uDCA7\uD83D\uDCA6☔️";
+                        break;
+                    case "/about":
+                        message_text =
+                                "\uD83C\uDF2A\uD83C\uDF08☀️\uD83C\uDF24⛅️\uD83C\uDF25☁️\uD83C\uDF26\uD83C\uDF27⛈\n" +
+                                "\n" +
+
+                                "Я Погодный Телеграм Бот! \n" +
+                                "Я знаю о погоде ВСЁ. \n" +
+                                "Напиши мне название города, \nв котором ты хочешь узнать погоду, \n" +
+                                "и я моментально дам тебе ответ!\n" +
+                                "\n" +
+                                "\uD83C\uDF29\uD83C\uDF28❄️☃️⛄️\uD83C\uDF2C\uD83D\uDCA8\uD83D\uDCA7\uD83D\uDCA6☔️";
+                        break;
+                    default:
+                        message_text = "http://api.openweathermap.org/data/2.5/weather?q=" + message_text + "&units=metric&lang=ru&appid=";
+                }
             }
+
+            try {
+                if (message_text.contains("http://api.openweathermap.org")) {
+                    message_text = Weather.getWeather(message_text, model);
+                    String photo_link = message_text.substring(0, 3);
+                    message_text = message_text.substring(3);
+                    SendPhoto foto = new SendPhoto().setChatId(chat_id).setPhoto(new File("C:\\Program Project\\WeatherTelegramBot\\src\\main\\resources\\icon\\" + photo_link.substring(0, 2) + ".png"));
+                    execute(foto);
+                }
+            } catch (IOException | TelegramApiException e) {
+                message_text = "К сожалению такой город не найден. \uD83D\uDE22 Проверь название города \uD83D\uDE1C";
+
+            }
+
 
             SendMessage message = new SendMessage() // Create a message object object
                     .setChatId(chat_id)
@@ -50,12 +90,14 @@ public class Bot extends TelegramLongPollingBot {
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
+
+
         }
 
     }
 
     public String getBotUsername() {
-        return "WorldWeatherTelegramBot";
+        return "LitTestBotMyBot";
     }
 
     public String getBotToken() {
